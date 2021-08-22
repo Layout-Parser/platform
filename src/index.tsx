376a85f73a69b8c320ea3561da9ca01b1ec068a7
lolipopshock <22512825+lolipopshock@users.noreply.github.com>
2021-08-22 18:52:52 -0400

    clean up app and index files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
