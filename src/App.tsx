1e9665db5c607f1b9e3e505d5d19dc9b08cc159a
lolipopshock <22512825+lolipopshock@users.noreply.github.com>
2021-08-22 23:52:45 -0400

    Add nav to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import "./styles/App.scss";
 import React from 'react';
+import { Nav } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          The Layout Parser Open Platform
-        </p>
-      </header>
-    </div>
+    <React.Fragment>
+      <Nav />
+      <div className="App">
+        <header className="App-header">
+          <p>
+            The Layout Parser Open Platform
+          </p>
+        </header>
+      </div>
+    </React.Fragment>
   );
 }
 
