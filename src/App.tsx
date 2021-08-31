db12656e9820ef84d01d5e922734c75e6e24b744
WeiningLi <bill.weining.li@gmail.com>
2021-08-31 16:22:16 -0400

    search function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./styles/App.scss";
 import React from "react";
-import { union } from "lodash";
+import { intersection, lowerCase, union } from "lodash";
 import {
   Hero,
   Container,
@@ -37,78 +37,6 @@ const NAVROUTES: NavRoute[] = [
   },
 ];
 
-// const SearchTagGroupsData: SearchTagRowProps[] = [
-//   {
-//     title: "Document Type",
-//     tags: [
-//       "Scientific", "Historical", "Forms"
-//     ]
-//   },
-//   {
-//     title: "Model Sizes",
-//     tags: [
-//       "Small", "Large"
-//     ]
-//   },
-//   {
-//     title: "Architecture",
-//     tags: [
-//       "Detectron2", "Paddle", "EfficientDet"
-//     ]
-//   }
-// ];
-
-// const AllModelData: ModelCardProps[] = [
-//   {
-//     name: "PubLayNet Models",
-//     author: "lp-official",
-//     docType: "Scientific",
-//     updateTime: "Aug 16, 2020",
-//     modelSpecs: [
-//       {
-//         modelConfig: "lp://PubLayNet/faster_rcnn_R_50_FPN_3x",
-//         tags: {
-//           backend: "detectron2",
-//           size: "large",
-//         }
-//       },
-//       {
-//         modelConfig: "lp://PubLayNet/lp://PubLayNet/mask_rcnn_R_50_FPN_3x/config",
-//         tags: {
-//           backend: "detectron2",
-//           size: "small",
-//         }
-//       }
-//     ],
-//     issueLink: 'https://www.google.com'
-//   },
-//   {
-//     name: "HJDataset Models",
-//     author: "lp-official",
-//     docType: "Historical",
-//     updateTime: "Aug 17, 2020",
-//     modelSpecs: [
-//       {
-//         modelConfig: "lp://HJDataset/mask_rcnn_R_50_FPN_3x/config",
-//         tags: {
-//           backend: "detectron2",
-//           size: "large",
-//         }
-//       }
-//     ],
-//     issueLink: 'https://www.baidu.com'
-//   },
-// ];
-
-// const AllPipelineData: ModelCardProps[] = [
-//   {
-//     name: "Table Detection Pipeline",
-//     author: "shannons",
-//     docType: "Business",
-//     updateTime: "Aug 16, 2021",
-//     issueLink: 'https://www.yahoo.com'
-//   },
-// ];
 
 function fetchSearchTagDataFromModelData(modelData: ModelCardProps[]) {
   return [
@@ -135,6 +63,47 @@ function fetchSearchTagDataFromModelData(modelData: ModelCardProps[]) {
       ),
     },
   ];
+}
+
+function applySearchConditions(
+  modelData: ModelCardProps[],
+  searchData: SearchData
+): ModelCardProps[] {
+  let targetModelData = modelData;
+
+  targetModelData = targetModelData.filter((model) =>
+    model.name.toLowerCase().includes(searchData.text.toLowerCase())
+  );
+
+  if (searchData.doctype.length) {
+    targetModelData = targetModelData.filter((model) => {
+      return intersection(model.docType, searchData.doctype).length;
+    });
+  }
+
+  if (searchData.backends.length) {
+    targetModelData = targetModelData.filter((model) => {
+      return intersection(
+        model.modelSpecs
+          ? model.modelSpecs.map((modelSpec) => modelSpec.tags.backend)
+          : [],
+        searchData.backends
+      ).length;
+    });
+  }
+
+  if (searchData.sizes.length) {
+    targetModelData = targetModelData.filter((model) => {
+      return intersection(
+        model.modelSpecs
+          ? model.modelSpecs.map((modelSpec) => modelSpec.tags.size)
+          : [],
+        searchData.sizes
+      ).length;
+    });
+  }
+
+  return targetModelData;
 }
 
 function App() {
@@ -167,9 +136,7 @@ function App() {
     });
   }
 
-  React.useEffect(() => {
-    console.log(searchData);
-  });
+  React.useEffect(() => { });
 
   return (
     <React.Fragment>
@@ -186,7 +153,10 @@ function App() {
                   searchTagRows={fetchSearchTagDataFromModelData(
                     modelData.concat(pipelineData)
                   )}
-                  totalModelCount={modelData.length + pipelineData.length}
+                  totalModelCount={
+                    applySearchConditions(modelData, searchData).length +
+                    applySearchConditions(pipelineData, searchData).length
+                  }
                   searchData={searchData}
                   setSearchData={setSearchData}
                 />
@@ -196,9 +166,11 @@ function App() {
                   Models
                 </Heading>
 
-                {modelData.map((singleModelData, i) => (
-                  <ModelCard key={i} {...singleModelData} />
-                ))}
+                {applySearchConditions(modelData, searchData).map(
+                  (singleModelData, i) => (
+                    <ModelCard key={i} {...singleModelData} />
+                  )
+                )}
 
                 <Heading
                   subtitle
@@ -210,9 +182,11 @@ function App() {
                   Pipelines
                 </Heading>
 
-                {pipelineData.map((singlePipelineData, i) => (
-                  <ModelCard key={i} {...singlePipelineData} />
-                ))}
+                {applySearchConditions(pipelineData, searchData).map(
+                  (singlePipelineData, i) => (
+                    <ModelCard key={i} {...singlePipelineData} />
+                  )
+                )}
               </Columns.Column>
             </Columns>
           </Container>
